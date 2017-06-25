2fff30d3408439f77363253c4fb2eb534fefdf0b
Oluyemi <kayoluyem23@gmail.com>
2017-06-25 05:24:15 +0100

    added preloader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,8 +22,20 @@
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
+<div id="preloader">
+    <div class="sk-cube-grid">
+        <div class="sk-cube sk-cube1"></div>
+        <div class="sk-cube sk-cube2"></div>
+        <div class="sk-cube sk-cube3"></div>
+        <div class="sk-cube sk-cube4"></div>
+        <div class="sk-cube sk-cube5"></div>
+        <div class="sk-cube sk-cube6"></div>
+        <div class="sk-cube sk-cube7"></div>
+        <div class="sk-cube sk-cube8"></div>
+        <div class="sk-cube sk-cube9"></div>
+    </div>
+</div>
     <div id="app">
-
         <!-- Modal for OTP resend or not received -->
         <div class="modal fade" id="noOTPModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog" id="" role="document">
@@ -281,8 +293,14 @@
     <!-- Custom script for all pages -->
     <script type="text/javascript" src="{{'js/script.js'}}"></script>
 
-    <!-- Show current date on the website -->
 
+    <script>
+        // Make preloader invisible
+        $(document).ready(function(){
+           $('#preloader').css('display', 'none');
+        });
+    </script>
+    <!-- Show current date on the website -->
     <script>
         var n = new Date();
             y = n.getFullYear();
